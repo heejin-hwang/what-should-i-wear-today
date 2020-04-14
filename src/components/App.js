b60925b0003d634d881607e79cbe4433805fa5b1
heejin-hwang <heyday.xz@gmail.com>
2020-04-14 15:49:55 +0900

    Make a skeleton code

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Weather from './Weather'
+
+class App extends Component {
+  render() {
+    return (
+    <>
+        <h1> logo </h1>
+        <Weather />
+    </>
+    );
+  }
+}
+
+export default App;
